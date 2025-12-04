8ef5c7a5854f57c9524751aab024dfb30100fb90
linktoahmad <linktoahmedkhalil@gmail.com>
2025-12-04 21:40:49 +0000

    fe be added

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/api/test', async (req, res) => {
+    try {
+        res.json({ message: "hello world" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+app.listen(3001, () => {
+    console.log('node running on port 3001');
+});
